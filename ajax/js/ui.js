56da8edff2165039637dc23d3b80ff017a62275a
munhyeonsuk <m01024038735@gmail.com>
2024-06-24 17:41:13 +0900

    hd 스크롤 js, scss

--- ajax/js/ui.js
+++ ajax/js/ui.js
@@ -3,9 +3,9 @@ $(function(){
         // $("body").attr("data-scrollps", $(this).scrollTop()) scrollTop 컨텐츠의 위치
         let scrpos = $(this).scrollTop();
         if(scrpos > 80){ //스크롤이 아래로
-            $("#hd").addClass('bg-dark border-bottom border-white').removeClass('bg-white')
+            $("#hd").addClass('scroll')
         } else {
-            $("#hd").removeClass('bg-dark border-bottom border-white').addClass('bg-white')
+            $("#hd").removeClass('scroll')
         }
     })
 })
